c7dda9166958309eb30b39870642fdcad11ebe16
Rasmus Nitsche <nitschedev@gmail.com>
2022-02-23 18:53:17 +0100

    Init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+	<router-view />
+</template>
+
+<script setup>
+/* JS */
+</script>
+
+<style lang="scss">
+/* CSS / SCSS */
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="Home">
+        <h1>{{ title }}</h1>
+    </div>
+</template>
+
+<script setup>
+/* JS */
+import { ref } from 'vue';
+const title = ref('Welcome to (vue 3-vite) application')
+</script>
+
+<style lang="scss" scoped>
+/* SCOPED CSS / SCSS */
+.Home {
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: #42b883;
+
+    h1 {
+        color: #35295e;
+        text-transform: uppercase;
+        font-weight: 400;
+        font-size: 36px;
+    }
+}
+</style>
